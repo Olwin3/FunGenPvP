189ed46290785175545981fbbce1088a34aa8d06
olwin1 <43321262+olwin1@users.noreply.github.com>
2018-09-17 07:43:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-    static void UpdatePresence()
-    {
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = "Playing FunGenPvP";
-        discordPresence.startTimestamp = 1507665886;
-        discordPresence.endTimestamp = 1507665886;
-        discordPresence.largeImageText = "Numbani";
-        discordPresence.smallImageText = "Rogue - Level 100";
-        discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-        discordPresence.partySize = 0;
-        discordPresence.partyMax = 0;
-        discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
-        discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
-        Discord_UpdatePresence(&discordPresence);
-    }
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
